7a011a165652bc5b332d201722867bb1d4ee2011
Wihhy <andre69tkachenko@ukr.net>
2023-01-26 18:49:04 +0200

    Lesson 12. Homework. Files

---
+++ Lesson_12/Decorator.py
@@ -0,0 +1,18 @@
+from datetime import datetime
+
+
+def name_and_time(input_func):
+    def inside_func():
+        with open('log.txt', mode='a') as log:
+            log.write(f'The function "{input_func.__name__}" was called at {datetime.now()}\n')
+        input_func()
+
+    return inside_func
+
+
+@name_and_time
+def printik():
+    print('Im inside the function')
+
+
+printik()
