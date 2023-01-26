418f7b78d4a43280b56815cc1ab3a0cd2de026a4
Wihhy <andre69tkachenko@ukr.net>
2023-01-26 18:33:57 +0200

    Lesson 11. Homework. Exceptions

--- Lesson_11/Exceptions.py
+++ Lesson_11/Exceptions.py
@@ -1,4 +1,21 @@
 import datetime
+import inspect
+
+
+def name_and_time(input_func):
+    def inside_func():
+        print(f'The function {input_func.__name__} was called at {datetime.datetime.now()}')
+        input_func()
+
+    return inside_func
+
+
+@name_and_time
+def printik():
+    print('im inside the function')
+
+
+printik()
 
 
 class MyCustomException(Exception):
