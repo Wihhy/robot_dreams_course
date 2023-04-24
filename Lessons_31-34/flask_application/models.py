7487e6dee1b63ccbacf8babd0c6a999965bac31e
Wihhy <andre69tkachenko@ukr.net>
2023-04-25 00:27:48 +0300

    Fixes

--- Lessons_31-34/flask_application/models.py
+++ Lessons_31-34/flask_application/models.py
@@ -6,10 +6,6 @@ class User(db.Model):
     first_name = db.Column(db.String, nullable=False)
     last_name = db.Column(db.String, nullable=False)
     age = db.Column(db.Integer)
-    purchases = db.relationship('Purchase', backref='user', lazy=True)
-
-    def __repr__(self):
-        return f'user {self.first_name}'
 
 
 class Book(db.Model):
@@ -18,19 +14,13 @@ class Book(db.Model):
     author = db.Column(db.String, nullable=False)
     year = db.Column(db.Integer)
     price = db.Column(db.Integer)
-    purchases = db.relationship('Purchase', backref='book', lazy=True)
-
-    def __repr__(self):
-        return f'user {self.title}'
 
 
 class Purchase(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    user = db.relationship('User')
     book_id = db.Column(db.Integer, db.ForeignKey('book.id'), nullable=False)
-
-    def __repr__(self):
-        return f'user {self.id}'
-
+    book = db.relationship('Book')
 
 
